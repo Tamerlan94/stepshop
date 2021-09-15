cbe24a818f1a6c37e24eca06540257174708370c
Tamerlan94 <66457450+Tamerlan94@users.noreply.github.com>
2021-09-15 20:55:31 +0600

    completed work lesson_04

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
+from .models import Product, ProductCategory
 
 
 def products(request):
     title = 'продукты | каталог'
-
-    links_menu = [
-        {'href': 'product_all', 'name': 'все'},
-        {'href': 'product_shoes', 'name': 'обувь'},
-        {'href': 'product_pants', 'name': 'штаны'},
-        {'href': 'product_phones', 'name': 'смартфоны'},
-    ]
-
+    products = Product.objects.all()
+    categories = ProductCategory.objects.all()
     context = {
         'title': title,
-        'links_menu': links_menu,
+        'products': products,
+        'categories': categories,
     }
 
     return render(request, 'mainapp/products.html', context)
--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, ProductCategory
+
 
 def index(request):
 
     title = 'главная'
+    categories = ProductCategory.objects.all()
+    products = Product.objects.all()
 
     context = {
         'title': title,
+        'categories': categories,
+        'products': products,
     }
 
     return render(request, 'stepshop/index.html', context)
