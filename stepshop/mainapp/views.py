755e4bbc2ba5921128e153dc5792d09afa9c23d3
Tamerlan94 <66457450+Tamerlan94@users.noreply.github.com>
2021-09-16 21:57:27 +0600

    completed work lesson_07

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, ProductCategory
 
 
-def products(request):
+def products(request, pk=None):
     title = 'продукты | каталог'
-    products = Product.objects.all()
+    products_all = Product.objects.all()
     categories = ProductCategory.objects.all()
+    category = {'name': 'продукты'}
+
+    if pk is not None:
+        products_all = Product.objects.filter(category__id=pk)
+        category = get_object_or_404(ProductCategory, id=pk)
+
     context = {
         'title': title,
-        'products': products,
+        'products': products_all,
         'categories': categories,
+        'category': category,
+        'pk': pk,
     }
 
     return render(request, 'mainapp/products.html', context)
